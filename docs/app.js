c7e5e05c87602f506d3644062cb411abe8479b6b
Ben Shealy <bentshermann@gmail.com>
2019-02-12 16:51:06 -0500

    Added code to start and stop server

--- docs/app.js
+++ docs/app.js
@@ -6,7 +6,7 @@ app.config(["$compileProvider", function($compileProvider) {
 	$compileProvider.debugInfoEnabled(false);
 }]);
 
-app.service("api", ["$http", "$q", function($http, $q) {
+app.service("api", ["$http", function($http) {
 	const self = this;
 
 	const httpRequest = function(method, url, params, data) {
@@ -21,7 +21,7 @@ app.service("api", ["$http", "$q", function($http, $q) {
 		}).then(function(res) {
 			return res.data;
 		}, function(res) {
-			return $q.reject(res.data);
+			return Promise.reject(res.data);
 		});
 	};
 
@@ -39,16 +39,16 @@ app.service("api", ["$http", "$q", function($http, $q) {
 		return httpRequest("get", "/v2/droplets/" + id);
 	};
 
-	this.Droplet.create = function(snapshot_id) {
+	this.Droplet.create = function(name, region, size, snapshot_id) {
 		return httpRequest("post", "/v2/droplets", {}, {
-			"name": "minecraft-server",
-			"region": "nyc3",
-			"size": "s-1vcpu-1gb",
+			"name": name,
+			"region": region,
+			"size": size,
 			"image": snapshot_id
 		});
 	};
 
-	this.Droplet.remove = function(id) {
+	this.Droplet.delete = function(id) {
 		return httpRequest("delete", "/v2/droplets/" + id);
 	};
 
@@ -62,6 +62,10 @@ app.service("api", ["$http", "$q", function($http, $q) {
 		return httpRequest("post", "/v2/droplets/" + droplet_id + "/actions", {}, { "type": "power_off" });
 	};
 
+	this.DropletAction.snapshot = function(droplet_id, name) {
+		return httpRequest("post", "/v2/droplets/" + droplet_id + "/actions", {}, { "type": "snapshot", "name": name });
+	};
+
 	this.DropletAction.get = function(droplet_id, id) {
 		return httpRequest("get", "/v2/droplets/" + droplet_id + "/actions/" + id);
 	};
@@ -70,6 +74,10 @@ app.service("api", ["$http", "$q", function($http, $q) {
 
 	this.Snapshot.query = function() {
 		return httpRequest("get", "/v2/snapshots?resource_type=droplet");
+	};
+
+	this.Snapshot.delete = function(id) {
+		return httpRequest("delete", "/v2/snapshots/" + id);
 	};
 }]);
 
@@ -85,39 +93,53 @@ app.controller("HomeCtrl", ["$scope", "$timeout", "api", function($scope, $timeo
 	$scope.initialize = async function(api_key) {
 		api.authenticate(api_key);
 
-		const promise1 = api.Droplet.query();
-		const promise2 = api.Snapshot.query();
+		let promise1 = api.Droplet.query();
+		let promise2 = api.Snapshot.query();
 
 		$scope.droplets = (await promise1).droplets;
 		$scope.snapshots = (await promise2).snapshots;
 	};
 
-	$scope.start = async function(snapshot_id) {
-		await api.Droplet.create(snapshot_id);
+	$scope.start = async function(snapshot) {
+		// create droplet
+		let result = await api.Droplet.create(snapshot.name, "nyc3", "s-1vcpu-1gb", snapshot.id);
+		let droplet_id = result.droplet.id;
 
 		$scope.status = "Starting...";
+
+		// wait for droplet creation to complete
+		while ( true ) {
+			let result = await api.Droplet.get(droplet_id);
+			let status = result.droplet.status;
+
+			if ( status === "active" ) {
+				break;
+			}
+			else {
+				await sleep(3000);
+			}
+		}
+
+		$scope.status = "Running";
+		$scope.$digest();
 	};
 
-	$scope.stop = async function(droplet_id, snapshot_id) {
+	$scope.stop = async function(droplet) {
 		if ( !confirm("Are you sure you want to stop the Minecraft server?") ) {
 			return;
 		}
 
 		// attempt to shutdown the droplet
-		const result = await api.DropletAction.shutdown(droplet_id);
-		const action_id = result.action.id;
-
 		$scope.status = "Shutting down...";
 
-		while ( true ) {
-			const result = await api.DropletAction.get(droplet_id, action_id);
-			const status = result.action.status;
+		let shutdownResult = await api.DropletAction.shutdown(droplet.id);
 
-			if ( status === "completed" ) {
-				break;
-			}
-			else if ( status === "errored" ) {
-				$scope.status = "Error: shutdown failed"
+		// wait for droplet to power off
+		while ( true ) {
+			let result = await api.Droplet.get(droplet.id);
+			let status = result.droplet.status;
+
+			if ( status === "off" ) {
 				break;
 			}
 			else {
@@ -125,10 +147,35 @@ app.controller("HomeCtrl", ["$scope", "$timeout", "api", function($scope, $timeo
 			}
 		}
 
-		// attempt to power off the droplet
-
 		// create snapshot of droplet
+		$scope.status = "Creating snapshot...";
 
-		// destroy old snapshot and droplet
+		let snapshotResult = await api.DropletAction.snapshot(droplet.id, droplet.name);
+		let action_id = snapshotResult.action.id;
+
+		// wait for snapshot to complete
+		while ( true ) {
+			let result = await api.DropletAction.get(droplet.id, action_id);
+			let status = result.action.status;
+
+			if ( status === "completed" ) {
+				break;
+			}
+			else {
+				await sleep(3000);
+			}
+		}
+
+		// destroy droplet and old snapshot
+		$scope.status = "Cleaning up...";
+
+		let promise1 = api.Droplet.delete(droplet.id);
+		let promise2 = api.Snapshot.delete(droplet.image.id);
+
+		await promise1;
+		await promise2;
+
+		$scope.status = "Stopped";
+		$scope.$digest();
 	};
 }]);
