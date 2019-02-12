3813f6716b23f03f8a00791e36d28e85935c47dd
Ben Shealy <bentshermann@gmail.com>
2019-02-12 11:12:10 -0500

    Convert spaces to tabs

--- docs/app.js
+++ docs/app.js
@@ -20,28 +20,28 @@ app.service("api", ["$http", "$q", function($http, $q) {
 		});
 	};
 
-  this.Droplet = {};
+	this.Droplet = {};
 
 	this.Droplet.query = function() {
-    return httpRequest("get", "/v2/droplets");
+		return httpRequest("get", "/v2/droplets");
 	};
 
 	this.Droplet.get = function(id) {
-    return httpRequest("get", "/v2/droplets/" + id);
+		return httpRequest("get", "/v2/droplets/" + id);
 	};
 
 	this.Droplet.shutdown = function(id) {
-    return httpRequest("post", "/v2/droplets/" + id + "/actions", {}, { type: "shutdown" });
+		return httpRequest("post", "/v2/droplets/" + id + "/actions", {}, { type: "shutdown" });
 	};
 
 	this.Droplet.remove = function(id) {
-    return httpRequest("delete", "/v2/droplets/" + id);
+		return httpRequest("delete", "/v2/droplets/" + id);
 	};
 
 	this.Snapshot = {};
 
 	this.Snapshot.query = function() {
-    return httpRequest("get", "/v2/snapshots?resource_type=droplet");
+		return httpRequest("get", "/v2/snapshots?resource_type=droplet");
 	};
 }]);
 
@@ -52,18 +52,18 @@ app.controller("HomeCtrl", ["$scope", "$q", "api", function($scope, $q, api) {
 	$scope.start = function(snapshot_id) {
 	};
 
-  $scope.stop = function(droplet_id, snapshot_id) {
+	$scope.stop = function(droplet_id, snapshot_id) {
 		if ( !confirm("Are you sure you want to stop the Minecraft server?") ) {
 			return;
-		}  
-  };
+		}
+	};
 
 	// initialize
-  $q.all([
-    api.Droplet.query(),
-    api.Snapshot.query()
-  ]).then(function(results) {
-    $scope.droplets = results[0];
-    $scope.snapshots = results[1];
-  });
+	$q.all([
+		api.Droplet.query(),
+		api.Snapshot.query()
+	]).then(function(results) {
+		$scope.droplets = results[0];
+		$scope.snapshots = results[1];
+	});
 }]);
