c02f2eed14ac853ef13f3594031d9bb25b97b652
Ben Shealy <bentshermann@gmail.com>
2019-02-12 20:15:02 -0500

    Updated download.sh

--- download.sh
+++ download.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-VERSION=$1
+VERSION="$1"
 
-if [ -z $VERSION ]; then
+if [[ -z $VERSION ]]; then
     echo "warning: no version specified, defaulting to 1.7.10"
     VERSION="1.7.10"
 fi
 
-URL="https://s3.amazonaws.com/Minecraft.Download/versions/$VERSION/minecraft_server.$VERSION.jar"
+if [[ $VERSION == "1.13.2" ]]; then
+    URL="https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar"
+else
+    URL="https://s3.amazonaws.com/Minecraft.Download/versions/$VERSION/minecraft_server.$VERSION.jar"
+fi
+
 FILENAME="minecraft_server.jar"
 
 rm -f $FILENAME
